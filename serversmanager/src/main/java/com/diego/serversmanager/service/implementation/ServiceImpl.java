161814ff708ddcda782581341cee85ac264569c2
diegocardosodev <diegocardososacramento@gmail.com>
2022-03-02 23:07:21 -0300

    path image ok

--- serversmanager/src/main/java/com/diego/serversmanager/service/implementation/ServiceImpl.java
+++ serversmanager/src/main/java/com/diego/serversmanager/service/implementation/ServiceImpl.java
@@ -70,7 +70,7 @@ public class ServiceImpl implements ServerService {
     private String setServerImageUrl() {
         String[] imageNames = {"server1.png", "server2.png", "server3.png", "server4.png"};
         log.info("seting image");
-        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/img/" + imageNames[new Random()
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image/" + imageNames[new Random()
                 .nextInt(4)]).toUriString();
     }
 
